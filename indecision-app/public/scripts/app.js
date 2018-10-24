991de24dc502c1c97a0a81a4ccc8493b7fb6209e
John Pawlak <johnpawlak@gmail.com>
2018-10-24 13:11:53 -0400

    indecision to-do app initialized

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+console.log('App.js is running!');
+
+var app = {
+    title: 'how to',
+    subtitle: 'eat',
+    options: ['One', 'Two']
+};
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    )
+);
+
+var user = {
+    name: 'Andrew',
+    age: 26,
+    location: 'Philadelphia'
+};
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+var userName = 'Mike';
+var age = 29;
+
+var title = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -0,0 +1,18 @@
+const square = function (x) {
+    return x * x;
+}
+
+const squareArrow = (x) => {
+    return x * x
+}
+
+const squareArrow2 = x => x * x;
+
+const getfirstName = (name) => {
+    return name.split(' ')[0]
+}
+
+const getfirstName2 = name => name.split(' ')[0];
+
+console.log(square(8));
+
