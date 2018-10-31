28e47e1517aa4d2197b9d9207767240e29c39ac1
John Pawlak <johnpawlak@gmail.com>
2018-10-31 10:02:26 -0400

    set up initial routing for new expense app

--- expensifyApp/src/app.js
+++ expensifyApp/src/app.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-ReactDOM.render(<p>this is my boilerplate</p>, document.getElementById('app'))
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+)
+const AddExpensePage = () => (
+    <div>
+        This is from my addExpense component
+    </div>
+)
+const EditExpensePage = () => (
+    <div>
+        This is from my EditExpensePage component
+    </div>
+)
+const HelpPage = () => (
+    <div>
+        This is from my HelpPage component
+    </div>
+)
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" exact component={ExpenseDashboardPage} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
 
 
 
